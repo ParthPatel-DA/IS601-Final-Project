ae75ab5dd7bacd752457787f3c9e3e2846bc3aa7
Parth Patel <parthpatel454500@gmail.com>
2024-04-30 23:10:19 -0400

    added orders apis

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from fastapi.exceptions import HTTPException
 import sqlite3
-from schemas import Customer, Item
+from schemas import Customer, Item, Order, OrderDetails
 
 class Response:
     def __init__(self, message, data, code):
@@ -118,3 +118,75 @@ async def delete_item(id: int, cur: sqlite3.Cursor = Depends(get_db_cursor)):
     cur.execute("DELETE FROM items WHERE id = ?;", (id,))
     return Response("Item deleted", None, 200)
 
+@app.get("/orders/{id}", tags=["Orders"])
+async def get_order(id: int, cur: sqlite3.Cursor = Depends(get_db_cursor)):
+    cur.execute("SELECT * FROM orders WHERE id = ?;", (id,))
+    data = cur.fetchone()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    
+    cur.execute("SELECT items.id, items.name, items.price FROM items JOIN item_list ON items.id = item_list.item_id WHERE item_list.order_id = ?;", (id,))
+    items = cur.fetchall()
+    items = [Item(id=item[0], name=item[1], price=item[2]) for item in items]
+    total = sum([item.price for item in items])
+    
+    return Response("Order found", OrderDetails(id=data[0], timestamp=data[1], cust_id=data[2], notes=data[3], itemDetails=items, total=total), 200)
+
+@app.post("/orders", tags=["Orders"])
+async def create_order(order: Order, cur: sqlite3.Cursor = Depends(get_db_cursor)):
+    cur.execute("SELECT * FROM customers WHERE id = ?;", (order.cust_id,))
+    data = cur.fetchone()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    
+    cur.execute("INSERT INTO orders (cust_id, notes) VALUES (?, ?);", (order.cust_id, order.notes))
+    order_id = cur.lastrowid
+
+    for item in order.items:
+        cur.execute("SELECT * FROM items WHERE id = ?;", (item,))
+        data = cur.fetchone()
+
+        if data is None:
+            raise HTTPException(status_code=404, detail="Item not found")
+        
+        cur.execute("INSERT INTO item_list (order_id, item_id) VALUES (?, ?);", (order_id, item))
+    
+    return Response("Order created", Order(id=order_id, cust_id=order.cust_id, notes=order.notes, items=order.items), 201)
+
+@app.put("/orders/{id}", tags=["Orders"])
+async def update_order(id: int, order: Order, cur: sqlite3.Cursor = Depends(get_db_cursor)):
+    cur.execute("SELECT * FROM orders WHERE id = ?;", (id,))
+    data = cur.fetchone()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    
+    cur.execute("UPDATE orders SET cust_id = ?, notes = ? WHERE id = ?;", (order.cust_id, order.notes, id))
+
+    cur.execute("SELECT item_id FROM item_list WHERE order_id = ?;", (id,))
+    items = cur.fetchall()
+    items = [item[0] for item in items]
+
+    for item in order.items:
+        if item not in items:
+            cur.execute("INSERT INTO item_list (order_id, item_id) VALUES (?, ?);", (id, item))
+
+    for item in items:
+        if item not in order.items:
+            cur.execute("DELETE FROM item_list WHERE order_id = ? AND item_id = ?;", (id, item))
+            
+    return Response("Order updated", Order(id=id, cust_id=order.cust_id, notes=order.notes, items=order.items), 200)
+
+@app.delete("/orders/{id}", tags=["Orders"])
+async def delete_order(id: int, cur: sqlite3.Cursor = Depends(get_db_cursor)):
+    cur.execute("SELECT * FROM orders WHERE id = ?;", (id,))
+    data = cur.fetchone()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    
+    cur.execute("DELETE FROM orders WHERE id = ?;", (id,))
+    cur.execute("DELETE FROM item_list WHERE order_id = ?;", (id,))
+    return Response("Order deleted", None, 200)
--- schemas.py
+++ schemas.py
@@ -9,3 +9,21 @@ class Item(BaseModel):
     id: int | None = None
     name: str
     price: float
+
+class Order(BaseModel):
+    id: int | None = None
+    cust_id: int
+    notes: str
+    items: list[int]
+
+class OrderDetails(BaseModel):
+    id: int | None = None
+    timestamp: str
+    cust_id: int
+    notes: str
+    itemDetails: list[Item] | None = []
+    total: float | None = None
+
+class ItemList(BaseModel):
+    order_id: int
+    item_id: int
